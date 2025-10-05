2e4b623771c30381ed8f82059336e96d14cc4980
Oskar Hofmann <oskar.hofmann@gmx.de>
2025-10-06 00:59:43 +0200

    Add class for individual tiles

--- Tile.cpp
+++ Tile.cpp
@@ -1,1 +1,25 @@
 #include "Tile.h"
+#include <string>
+
+int Tile::getNum()
+{
+	return number;
+}
+
+bool Tile::isEmpty()
+{
+	return number == 0;
+}
+
+std::ostream& operator<<(std::ostream& out, const Tile& tile)
+{
+	std::string tile_print{ " " };
+	if (tile.number <= 9) {
+		tile_print += " ";
+	}
+	tile_print += std::to_string(tile.number);
+	tile_print += " ";
+
+	out << tile_print;
+	return out;
+}
--- Tile.h
+++ Tile.h
@@ -1,5 +1,21 @@
 #pragma once
+#include <utility>
+#include <iostream>
+
 class Tile
 {
+public:
+	int getNum();
+	bool isEmpty();
+
+	Tile() = default;
+	explicit Tile(int num) : number{ num } {};
+
+
+	friend std::ostream& operator<<(std::ostream& out, const Tile& tile);
+
+
+private:
+	int number{ 0 };
 };
 
